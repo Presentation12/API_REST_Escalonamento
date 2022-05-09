8537401db593ba62cb06b0b1fe60c76a94cc1e65
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2022-05-09 14:02:53 +0100

    Update CRUD Simulação + 0.5SimJob

--- Escalonamento/WebAPI/Controllers/SimJobController.cs
+++ Escalonamento/WebAPI/Controllers/SimJobController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Escalonamento.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +13,153 @@ namespace Escalonamento.Controllers
     [ApiController]
     public class SimJobController : ControllerBase
     {
-        // GET: api/<SimJobController>
+        #region GET
+
+        /// <summary>
+        /// Método que retorna todas as ligações entre um job e uma simulação
+        /// </summary>
+        /// <returns> Lista de SimJobs </returns>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<SimJob> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                using (var context = new EscalonamentoContext()) 
+                { 
+                    return context.SimJob.ToList(); 
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
         }
 
-        // GET api/<SimJobController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        /// <summary>
+        /// Método que retorna uma certa ligação entre um job e uma simulação
+        /// </summary>
+        /// <param name="idsim"> ID da simulação </param>
+        /// <param name="idjob"> ID do job </param>
+        /// <returns></returns>
+        [HttpGet("{idsim}_{idjob}")]
+        public SimJob Get(int idsim, int idjob)
         {
-            return "value";
+            try
+            {
+                using (var context = new EscalonamentoContext())
+                {
+                    return context.SimJob.Where(sj => sj.IdSim == idsim && sj.IdJob == idjob).FirstOrDefault();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
         }
 
-        // POST api/<SimJobController>
+        #endregion
+
+        #region POST
+
+        /// <summary>
+        /// Método que adiciona uma nova ligação entre job e simulação
+        /// </summary>
+        /// <param name="sj"> Informação do SimJob </param>
+        /// <returns> Estado do Método </returns>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public JsonResult Post([FromBody] SimJob sj)
         {
+            try
+            {
+                using (var context = new EscalonamentoContext())
+                {
+                    SimJob simJob = new SimJob();
+
+                    simJob.IdJobNavigation = context.Job.Where(j => j.IdJob == sj.IdJob).FirstOrDefault();
+                    simJob.IdSimNavigation = context.Simulacao.Where(s => s.IdSim == sj.IdSim).FirstOrDefault();
+
+                    context.SimJob.Add(simJob);
+
+                    context.SaveChanges();
+                    return new JsonResult("SimJob adicionado com sucesso!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new JsonResult("Erro");
+            }
         }
 
-        // PUT api/<SimJobController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        #endregion
+
+        //Provavelmente teremos de remover
+        #region PATCH
+
+        /// <summary>
+        /// Método que atualiza a informação de uma ligação entre job e simulação
+        /// </summary>
+        /// <param name="idsim"> ID da simulação </param>
+        /// <param name="idjob"> ID do job </param>
+        /// <param name="sj"> Informação da ligação SimJob </param>
+        /// <returns> Estado do método </returns>
+        [HttpPatch("{idsim}_{idjob}")]
+        public JsonResult Patch(int idsim, int idjob, [FromBody] SimJob sj)
         {
+            try
+            {
+                using (var context = new EscalonamentoContext())
+                {
+                    SimJob simJob = context.SimJob.Where(sjob => sjob.IdSim == idsim && sjob.IdJob == idjob).FirstOrDefault();
+
+                    if(sj.IdJob != 0) simJob.IdJobNavigation = context.Job.Where(j => j.IdJob == sj.IdJob).FirstOrDefault();
+                    if (sj.IdSim != 0) simJob.IdSimNavigation = context.Simulacao.Where(s => s.IdSim == sj.IdSim).FirstOrDefault();
+
+                    context.SaveChanges();
+                    return new JsonResult("SimJob alterado com sucesso!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new JsonResult("Erro");
+            }
         }
 
-        // DELETE api/<SimJobController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        #endregion
+
+        #region DELETE
+
+        /// <summary>
+        /// Método que remove uma ligação entre job e simulação da base de dados
+        /// </summary>
+        /// <param name="idsim"> ID da simulação </param>
+        /// <param name="idjob"> ID do job </param>
+        /// <returns> Estado do método </returns>
+        [HttpDelete("{idsim}_{idjob}")]
+        public JsonResult Delete(int idsim, int idjob)
         {
+            try
+            {
+                using (var context = new EscalonamentoContext())
+                {
+                    SimJob simJob = context.SimJob.Where(sj => sj.IdSim == idsim && sj.IdJob == idjob).FirstOrDefault();
+
+                    context.SimJob.Remove(simJob);
+
+                    context.SaveChanges();
+                    return new JsonResult("SimJob removido com sucesso!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new JsonResult("Erro");
+            }
         }
+
+        #endregion
     }
 }
--- Escalonamento/WebAPI/Controllers/SimulacaoController.cs
+++ Escalonamento/WebAPI/Controllers/SimulacaoController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Escalonamento.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +13,150 @@ namespace Escalonamento.Controllers
     [ApiController]
     public class SimulacaoController : ControllerBase
     {
-        // GET: api/<SimulacaoController>
+
+        #region GET
+
+        /// <summary>
+        /// Método que retorna lista de simulações
+        /// </summary>
+        /// <returns> Lista de simulações </returns>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Simulacao> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                using (var context = new EscalonamentoContext())
+                {
+                    return context.Simulacao.ToList();
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
         }
 
-        // GET api/<SimulacaoController>/5
+        /// <summary>
+        /// Método que retorna uma simulação em específico
+        /// </summary>
+        /// <param name="id"> ID da simulação </param>
+        /// <returns> Simulação </returns>
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Simulacao Get(int id)
         {
-            return "value";
+            try
+            {
+                using (var context = new EscalonamentoContext())
+                {
+                    return context.Simulacao.Where(s => s.IdSim == id).FirstOrDefault();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
         }
 
-        // POST api/<SimulacaoController>
+        #endregion
+
+        #region POST
+
+        /// <summary>
+        /// Método que adiciona uma simulação à base de dados
+        /// </summary>
+        /// <param name="sim"> Informação da simulação </param>
+        /// <returns> Resultado do método </returns>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public JsonResult Post([FromBody] Simulacao sim)
         {
+            try
+            {
+                using (var context = new EscalonamentoContext())
+                {
+                    Simulacao simulacao = new Simulacao();
+
+                    simulacao.IdSim = sim.IdSim;
+                    simulacao.Estado = sim.Estado;
+                    simulacao.IdUserNavigation = context.Utilizador.Where(u => u.IdUser == sim.IdUser).FirstOrDefault();
+
+                    context.Simulacao.Add(simulacao);
+
+                    context.SaveChanges();
+                    return new JsonResult("Simulacao adicionada com sucesso!");
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return new JsonResult("Erro");
+            }
         }
 
-        // PUT api/<SimulacaoController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        #endregion
+
+        #region PATCH
+
+        /// <summary>
+        /// Método que atualiza a informação de uma certa simulação
+        /// </summary>
+        /// <param name="id"> ID da simulação </param>
+        /// <param name="sim"> Informação da simulação </param>
+        /// <returns> Estado do método </returns>
+        [HttpPatch("{id}")]
+        public JsonResult Patch(int id, [FromBody] Simulacao sim)
         {
+            try
+            {
+                using (var context = new EscalonamentoContext())
+                {
+                    Simulacao simulacao = context.Simulacao.Where(s => s.IdSim == id).FirstOrDefault();
+
+                    simulacao.IdSim = sim.IdSim == 0 ? simulacao.IdSim : sim.IdSim;
+                    simulacao.Estado = sim.Estado is null ? simulacao.Estado : sim.Estado;
+                    if(sim.IdUser != 0) simulacao.IdUserNavigation = context.Utilizador.Where(u => u.IdUser == sim.IdUser).FirstOrDefault();
+
+                    context.SaveChanges();
+                    return new JsonResult("Simulacao alterada com sucesso!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new JsonResult("Erro");
+            }
         }
 
-        // DELETE api/<SimulacaoController>/5
+        #endregion
+
+        #region DELETE
+
+        /// <summary>
+        /// Método que elimina uma certa simulação da base de dados
+        /// </summary>
+        /// <param name="id"> ID da simulação </param>
+        /// <returns> Estado do método </returns>
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public JsonResult Delete(int id)
         {
+            try
+            {
+                using (var context = new EscalonamentoContext())
+                {
+                    Simulacao sim = context.Simulacao.Where(s => s.IdSim == id).FirstOrDefault();
+
+                    context.Simulacao.Remove(sim);
+
+                    context.SaveChanges();
+                    return new JsonResult("Simulação removida com sucesso!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
         }
+
+        #endregion
     }
 }
