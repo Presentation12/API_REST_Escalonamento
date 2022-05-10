e3d087fa259a41870e2246d2f8b0dc36f7874071
Presentation12 <joaocarlosapresentacao@gmail.com>
2022-05-10 21:58:15 +0100

    Frontend updates

--- Frontend/pagina/js/pagina.js
+++ Frontend/pagina/js/pagina.js
@@ -9,3 +9,27 @@ function selectSim() {
         document.getElementById("ref").href = "sim.html";
     }
 }
+
+document.getElementById("btnOp").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("novaOp").style.display = "flex";
+
+})
+
+document.getElementById("btnSim").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("simulacao").style.display = "flex";
+    document.getElementById("btnSim").style.display = "none";
+})
+
+document.getElementById("btnCancelar").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("simulacao").style.display = "none";
+    document.getElementById("btnSim").style.display = "";
+})
+
+document.getElementById("cancel").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("novaOp").style.display = "none";
+
+})
